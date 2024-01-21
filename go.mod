b89bb5a59e67d5bdc4204d8e45c40b13f80a32a6
crhuber <crhuber@users.noreply.github.com>
2024-01-21 18:16:23 +0100

    add urfave cli

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 require (
 	github.com/andybalholm/brotli v1.0.0 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dsnet/compress v0.0.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
@@ -38,11 +39,14 @@ require (
 	github.com/pierrec/lz4/v4 v4.0.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/spf13/afero v1.9.5 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
+	github.com/urfave/cli/v2 v2.27.1 // indirect
 	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/term v0.13.0 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,359 @@
 package main
 
 import (
-	"crhuber/kelp/cmd"
+	"crhuber/kelp/pkg/config"
+	"crhuber/kelp/pkg/install"
+	"crhuber/kelp/pkg/utils"
+	"errors"
+	"fmt"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/urfave/cli/v2"
 )
 
-var version = "1.11.0"
+var version = "1.12.0"
 
 func main() {
-	rootCmd := cmd.NewRootCmd(version)
 
-	if err := rootCmd.Execute(); err != nil {
+	// default config
+	var home, _ = os.UserHomeDir()
+	var KelpConf = filepath.Join(home, "/.kelp/kelp.json")
+
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "config",
+				Aliases: []string{"c"},
+				Value:   KelpConf,
+				Usage:   "path to kelp config file",
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:  "add",
+				Usage: "add a new package to kelp config",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:    "release",
+						Aliases: []string{"r"},
+						Value:   "latest",
+						Usage:   "release for package",
+					},
+				},
+				Action: func(cCtx *cli.Context) error {
+
+					project := cCtx.Args().First()
+					ownerRepo := strings.Split(project, "/")
+					if len(ownerRepo) < 2 {
+						return fmt.Errorf("use owner/repo format")
+
+					}
+
+					// load config
+					kc, err := config.Load(cCtx.String("config"))
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+
+					err = kc.AddPackage(ownerRepo[0], ownerRepo[1], cCtx.String("release"))
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+					// save config
+					err = kc.Save()
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+
+					return nil
+				},
+			},
+			{
+				Name:  "browse",
+				Usage: "browse to project github page",
+				Action: func(cCtx *cli.Context) error {
+					project := cCtx.Args().First()
+					if project == "" {
+						return errors.New("project argument required")
+					}
+
+					// load config
+					kc, err := config.Load(cCtx.String("config"))
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+
+					p, err := kc.GetPackage(project)
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+					config.Browse(p.Owner, p.Repo)
+					return nil
+				},
+			},
+			{
+				Name:  "doctor",
+				Usage: "checks if kelp packages are installed properly",
+				Action: func(cCtx *cli.Context) error {
+					// load config
+					kc, err := config.Load(cCtx.String("config"))
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+					kc.Doctor()
+					return nil
+
+				},
+			},
+			{
+				Name:  "get",
+				Usage: "get package details",
+				Action: func(cCtx *cli.Context) error {
+
+					project := cCtx.Args().First()
+					if project == "" {
+						return errors.New("project argument required")
+					}
+
+					// load config
+					kc, err := config.Load(cCtx.String("config"))
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+
+					p, err := kc.GetPackage(project)
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+
+					fmt.Printf("\n[%s/%s]", p.Owner, p.Repo)
+					fmt.Printf("\nRelease: %s", p.Release)
+					fmt.Printf("\nDescription: %s", p.Description)
+					fmt.Printf("\nUrl: https://github.com/%s/%s", p.Owner, p.Repo)
+					fmt.Printf("\nBinary: %s", p.Binary)
+					fmt.Printf("\nUpdated At: %s", p.UpdatedAt)
+					return nil
+				},
+			},
+			{
+				Name:  "init",
+				Usage: "initialize kelp",
+				Action: func(cCtx *cli.Context) error {
+					// load config
+					kc, err := config.Load(cCtx.String("config"))
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+					// save config
+					err = kc.Save()
+					if err != nil {
+						return fmt.Errorf("error saving: %s", err)
+					}
+					return nil
+				},
+			},
+			{
+				Name:  "inspect",
+				Usage: "inspect kelp bin directory",
+				Action: func(cCtx *cli.Context) error {
+					config.Inspect()
+					return nil
+				},
+			},
+			{
+				Name:  "install",
+				Usage: "install kelp package",
+				Action: func(cCtx *cli.Context) error {
+					project := cCtx.Args().First()
+					if project == "" {
+						return errors.New("project argument required")
+					}
+
+					// load config
+					kc, err := config.Load(cCtx.String("config"))
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+					kp, err := kc.GetPackage(project)
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+					err = install.Install(kp.Owner, kp.Repo, kp.Release)
+					if err != nil {
+						return err
+					}
+
+					return nil
+				},
+			},
+			{
+				Name:    "list",
+				Aliases: []string{"ls"},
+				Usage:   "list kelp packages",
+				Action: func(cCtx *cli.Context) error {
+					// load config
+					kc, err := config.Load(cCtx.String("config"))
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+					kc.List()
+					return nil
+				},
+			},
+			{
+				Name:    "remove",
+				Aliases: []string{"rm"},
+				Usage:   "remove a packages from config and disk",
+				Action: func(cCtx *cli.Context) error {
+					project := cCtx.Args().First()
+					if project == "" {
+						return errors.New("project argument required")
+					}
+
+					// load config
+					kc, err := config.Load(cCtx.String("config"))
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+					kp, err := kc.GetPackage(project)
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+
+					// remove from config
+					err = kc.RemovePackage(kp.Repo)
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+
+					// save config
+					err = kc.Save()
+					if err != nil {
+						return fmt.Errorf("error saving: %s", err)
+					}
+					return nil
+				},
+			},
+			{
+				Name:  "set",
+				Usage: "set package configuration in kelp config",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:    "release",
+						Aliases: []string{"r"},
+						Value:   "latest",
+						Usage:   "release for package",
+					},
+					&cli.StringFlag{
+						Name:    "description",
+						Aliases: []string{"d"},
+						Value:   "",
+						Usage:   "description of package",
+					},
+					&cli.StringFlag{
+						Name:    "binary",
+						Aliases: []string{"b"},
+						Value:   "",
+						Usage:   "alias of binary",
+					},
+				},
+				Action: func(cCtx *cli.Context) error {
+					project := cCtx.Args().First()
+					if project == "" {
+						return errors.New("project argument required")
+					}
+
+					// load config
+					kc, err := config.Load(cCtx.String("config"))
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+
+					err = kc.SetPackage(project, cCtx.String("release"), cCtx.String("description"), cCtx.String("binary"))
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+					// save config
+					err = kc.Save()
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+
+					return nil
+				},
+			},
+			{
+				Name:  "update",
+				Usage: "update kelp packge",
+				Action: func(cCtx *cli.Context) error {
+					project := cCtx.Args().First()
+					if project == "" {
+						return errors.New("project argument required")
+					}
+
+					// load config
+					kc, err := config.Load(cCtx.String("config"))
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+					kp, err := kc.GetPackage(project)
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+
+					// handle http packages
+					if strings.HasPrefix(kp.Release, "http") {
+						return errors.New("update functionality not supported for http packages")
+					}
+
+					ghr, err := utils.GetGithubRelease(kp.Owner, kp.Repo, "latest")
+					if err != nil {
+						return fmt.Errorf("%s", err)
+					}
+
+					if ghr.TagName == kp.Release {
+						fmt.Printf("Latest release %s already matches release %s in kelp config", ghr.TagName, kp.Release)
+						return nil
+					}
+
+					fmt.Printf("Latest release %s. Kelp configured release %s. Update config [y/n] ? : ", ghr.TagName, kp.Release)
+
+					var confirmation string
+					confirmation = strings.TrimSpace(confirmation)
+					confirmation = strings.ToLower(confirmation)
+
+					// Taking input from user
+					fmt.Scanln(&confirmation)
+					if confirmation == "y" || confirmation == "yes" {
+						err = kc.SetPackage(kp.Repo, ghr.TagName, "", "")
+						if err != nil {
+							return fmt.Errorf("%s", err)
+						}
+						// save config
+						err = kc.Save()
+						if err != nil {
+							return fmt.Errorf("%s", err)
+						}
+					}
+
+					return nil
+				},
+			},
+			{
+				Name:  "version",
+				Usage: "show kelp version",
+				Action: func(cCtx *cli.Context) error {
+					fmt.Println("kelp version ", version)
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
 }
